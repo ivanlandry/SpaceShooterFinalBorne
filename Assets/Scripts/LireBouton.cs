5e5f359fe530868c38a14e8ec45fc53c9020d5f2
LaCoors <dave.lacourse@cegeptr.qc.ca>
2022-12-01 10:55:15 -0500

    Initial commit

---
+++ Assets/Scripts/LireBouton.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LireBouton : MonoBehaviour
+{
+    private Button _button;
+    private HighScoreTable _highScoreTable;
+
+    private void Start()
+    {
+        _highScoreTable = FindObjectOfType<HighScoreTable>();
+        _button = this.GetComponent<Button>();
+        _button.onClick.AddListener(LireTexte);
+    }
+
+    public void LireTexte()
+    {
+        //Debug.Log(this.GetComponentInChildren<TMP_Text>().text);
+        if (_highScoreTable != null)
+        {
+            _highScoreTable.AjouterLettre(this.GetComponentInChildren<TMP_Text>().text);
+        }
+    }
+
+}
